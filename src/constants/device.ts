73bfa99b004aa43e1c56c241cf69bb0fc72679fb
Ssuyu Ke <ssuyuke@asy.tw>
2025-03-22 12:56:37 +0800

    ✨ feat:(custom): add map at home page

--- src/constants/device.ts
+++ src/constants/device.ts
@@ -2,6 +2,7 @@ import { generatePath } from '@/utils/url'
 
 export const deviceAPI = {
   DEVICE_LIST: '/api/Device/List',
+  DEVICE_NEARBY: '/api/Device/Nearby',
   DEVICE: (DeviceId: number) =>
     generatePath('/api/Device/Device/:DeviceId', { DeviceId })
 }
--- src/types/api/device.ts
+++ src/types/api/device.ts
@@ -22,3 +22,34 @@ export interface DeviceItem {
   online: 'Y' | 'N' // 假設 online 只有這兩個可能的值
   registerDate: string // 或者可以使用 Date 類型，取決於你如何使用這個字段
 }
+
+export interface DeviceNearbyInfo {
+  lat: number
+  lng: number
+  radius: number
+}
+
+export interface DeviceItem {
+  deviceId: number
+  name: string
+  companyId: string
+  companyName: string
+  mac: string
+  city: string
+  town: string
+  address: string
+  lat: number
+  lng: number
+  version: string
+  token: string
+  resolutionW: number
+  resolutionH: number
+  volume: number
+  camera: string
+  tag: string | null
+  remark: string | null
+  access: 'Private' | 'Public'
+  status: 'Enable' | 'Disable'
+  online: 'Y' | 'N'
+  registerDate: string
+}
